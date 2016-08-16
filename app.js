3741e194ec0f3f538ae602d3e74fa8fd125bef69
Brian <brianallen5193@gmail.com>
2016-08-15 21:16:49 -0400

    Adding a landing page to host the survey questions

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var path    = require("path");
 
 app.get('/', function (req, res) {
-  res.send('Howdy');
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Sumo Me Webserver listening on port 3000!');
 });
